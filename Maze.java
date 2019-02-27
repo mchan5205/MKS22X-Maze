2a20865475b472118ed501d6579c30db5e4cc932
Michael Chan <mchan10@stuy.edu>
2019-02-27 10:36:10 -0500

    added constructor to Maze

--- Maze.java
+++ Maze.java
@@ -4,7 +4,23 @@ public class Maze{
   private char[][]maze;
   private boolean animate;
   public Maze(String filename) throws FileNotFoundException{
+    String line;
+    int i;
     File f = new File(filename);
     Scanner s = new Scanner(f);
-    
+    while(s.hasNextLine()){
+      line = s.nextLine();
+      i++;
+    }
+    s.reset();
+    i = 0;
+    maze = new char[line.length][i];
+    while(s.hasNextLine()){
+      line = s.nextLine();
+      for (int x = 0; x < line.length; x++){
+        maze[x][i] = line.charAt(x);
+      }
+      i++
+    }
+    animate = false;
   }
