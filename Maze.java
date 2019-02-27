2a78e91dd5f5986b42a8f98e83920b1113b74d7e
Michael Chan <mchan10@stuy.edu>
2019-02-27 10:27:50 -0500

    added maze file and started constructor

---
+++ Maze.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class Maze{
+  private char[][]maze;
+  private boolean animate;
+  public Maze(String filename) throws FileNotFoundException{
+    File f = new File(filename);
+    Scanner s = new Scanner(f);
+    
+  }
