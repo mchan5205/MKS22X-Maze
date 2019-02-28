771189fd7cf374029be7d452a67deb4ea16514af
Michael Chan <mchan10@stuy.edu>
2019-02-28 10:41:53 -0500

    added recursive solve

--- Maze.java
+++ Maze.java
@@ -38,11 +38,41 @@ public class Maze{
     return solve(sx, sy);
   }
   private int solve(int row, int col){
+    int fin;
     if(animate){
       clearTerminal();
       System.out.println(this);
       wait(20);
     }
-
+    if (maze[row][col] == 'E'){
+      return ;
+    }
+    maze[row][col] = '@';
+    if (maze[row + 1][col] != 'x' || maze[row + 1][col] != '@' || maze[row + 1][col] != '.'){
+      fin = solve[row + 1][col];
+      if (fin != -1){
+        return fin;
+      }
+    }
+    if (maze[row - 1][col] != 'x' || maze[row - 1][col] != '@' || maze[row - 1][col] != '.'){
+      fin = solve[row - 1][col];
+      if (fin != -1){
+        return fin;
+      }
+    }
+    if (maze[row][col + 1] != 'x' || maze[row][col + 1] != '@' || maze[row][col + 1] != '.'){
+      fin = solve[row][col + 1];
+      if (fin != -1){
+        return fin;
+      }
+    }
+    if (maze[row][col - 1] != 'x' || maze[row][col - 1] != '@' || maze[row][col - 1] != '.'){
+      fin = solve[row][col - 1];
+      if (fin != -1){
+        return fin;
+      }
+    }
+    maze[row][col] = '.';
+    return -1;
   }
 }
